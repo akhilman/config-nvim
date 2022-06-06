55d761cce2440057c6b9ebed2e715d34098f81fb
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-07 03:12:31 +0700

    Disable unused language syntax plugins

--- lua/plug-syntax.lua
+++ lua/plug-syntax.lua
@@ -3,15 +3,15 @@ local M = {}
 function M.startup(use)
   use 'cstrahan/vim-capnp'
   use 'dcharbon/vim-flatbuffers'
-  use 'djpohly/vim-execline'
-  use 'gluon-lang/vim-gluon'
-  use 'teal-language/vim-teal'
   -- We are using Treesitter for syntax highlight
   -- use 'cespare/vim-toml'
   -- use 'dag/vim-fish'
+  -- use 'djpohly/vim-execline'
+  -- use 'gluon-lang/vim-gluon'
   -- use 'jonsmithers/vim-html-template-literals'
   -- use 'leafgarland/typescript-vim'
   -- use 'pangloss/vim-javascript'
+  -- use 'teal-language/vim-teal'
   -- use { 'andys8/vim-elm-syntax', { ft = 'elm' } }
 end
 
