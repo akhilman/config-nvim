e0cdf58ea000b58537204abc0209b4ca33ec5b09
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-08 14:20:35 +0700

    Split telescope keymap by categories

--- lua/plug-telescope.lua
+++ lua/plug-telescope.lua
@@ -4,19 +4,23 @@ local function setup()
   local telescope_builtin = require 'telescope.builtin'
   local opts = { silent = true }
 
+  -- Buffers
+  vim.keymap.set('n', '<leader>f/', telescope_builtin.current_buffer_fuzzy_find, opts)
+  vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, opts)
+
   -- Diagnostics
   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<space>q', telescope_builtin.diagnostics, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<space>q', telescope_builtin.diagnostics, opts)
 
   -- Files
   vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, opts)
   vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, opts)
   vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, opts)
-  vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, opts)
   vim.keymap.set('n', '<leader>fs', telescope_builtin.grep_string, opts)
-  vim.keymap.set('n', '<leader>f/', telescope_builtin.current_buffer_fuzzy_find, opts)
+
+  -- Telescope history
   vim.keymap.set('n', '<leader>fh', telescope_builtin.pickers, opts)
 end
 
