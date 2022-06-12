7c18724b195db6918931b8d0ae519faba9bc9330
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-12 12:32:01 +0700

    Move default color schema tweaks to vim file

--- lua/conf-colorscheme.lua
+++ lua/conf-colorscheme.lua
@@ -3,22 +3,7 @@
 local M = {}
 
 local function default_color_scheme()
-  vim.api.nvim_exec [[
-"""
-" Tweaked default colour scheme
-""
-
-colorscheme default
-set background=dark
-highlight Pmenu ctermfg=white ctermbg=darkgray guifg=#aaaaaa guibg=#222222
-highlight PmenuSel ctermfg=white ctermbg=blue guifg=white guibg=#194675
-highlight Visual term=reverse cterm=reverse ctermbg=black guifg=#ffffff guibg=#194675
-highlight Folded guibg=gray30 guifg=cyan
-highlight FoldColumn guibg=darkgrey guifg=white
-highlight SpellBad cterm=undercurl
-highlight VertSplit term=bold cterm=bold gui=bold ctermbg=darkgray guifg=Gray20 guibg=Gray5
-highlight LineNr ctermfg=gray guifg=gray
-]]
+  vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/colorscheme.vim')
 end
 
 function M.setup()
