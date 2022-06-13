0c21e1bfb28780d08e6c89ae569ddf1a621c3693
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-14 02:43:18 +0700

    Map right mouse button for nvim-qt

--- lua/conf-gui.lua
+++ lua/conf-gui.lua
@@ -7,6 +7,13 @@ local function do_setup()
   if vim.fn.exists(':GuiTabline') ~= 0 then
     vim.cmd('GuiTabline 0')
   end
+
+  if vim.fn.exists('*GuiShowContextMenu') ~= 0 then
+    vim.keymap.set('n', '<RightMouse>', ':call GuiShowContextMenu()<CR>', { silent = true })
+    vim.keymap.set('i', '<RightMouse>', '<Esc>:call GuiShowContextMenu()<CR>', { silent = true })
+    vim.keymap.set('x', '<RightMouse>', ':call GuiShowContextMenu()<CR>gv', { silent = true })
+    vim.keymap.set('s', '<RightMouse>', '<C-G>:call GuiShowContextMenu()<CR>gv', { silent = true })
+  end
 end
 
 function M.setup()
