e075c3f8d948181e27a216f8ad8bf3e0f5eab4bd
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-06-14 12:37:52 +0700

    Show current session name in lualine

--- lua/plug-lualine.lua
+++ lua/plug-lualine.lua
@@ -1,16 +1,23 @@
 -- Fancier statusline
-
 local M = {}
 
 local function setup()
+  local plugins = require 'plugins'
+
+  local tabline = {
+    lualine_a = { 'tabs', 'buffers' },
+    lualine_z = {},
+  }
+  if plugins.is_enabled('plug-auto-session') then
+    table.insert(tabline.lualine_z, require('auto-session-library').current_session_name)
+  end
+
   --Set statusbar
   require('lualine').setup {
     options = {
       theme = 'auto',
     },
-    tabline = {
-      lualine_a = {'tabs', 'buffers'}
-    }
+    tabline = tabline,
   }
 end
 
