c9a827f899b92065ccff0c025cadc337e39a1217
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-07-21 11:23:28 +0700

    Show file's full path in the status line

--- lua/plug-lualine.lua
+++ lua/plug-lualine.lua
@@ -12,12 +12,21 @@ local function setup()
     table.insert(tabline.lualine_z, require('auto-session-library').current_session_name)
   end
 
+  local sections = {
+    lualine_c = { { 'filename', path = 1 } }
+  }
+
+  local inactive_sections = {
+    lualine_c = { { 'filename', path = 1 } }
+  }
   --Set statusbar
   require('lualine').setup {
     options = {
       theme = 'auto',
     },
     tabline = tabline,
+    sections = sections,
+    inactive_sections = inactive_sections,
   }
 end
 
