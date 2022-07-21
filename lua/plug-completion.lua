df40e63cb9920ddf3be23b586a6dde734dcb037e
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-07-21 11:24:16 +0700

    Jump between completion expansions by <C-l> and <C-h>

--- lua/plug-completion.lua
+++ lua/plug-completion.lua
@@ -41,7 +41,7 @@ function M.packer_setup()
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
-      ['<Tab>'] = cmp.mapping(function(fallback)
+      ['<C-l>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
@@ -50,7 +50,7 @@ function M.packer_setup()
           fallback()
         end
       end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
+      ['<C-h>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
