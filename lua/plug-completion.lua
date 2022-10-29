1f9aa04dfba0dfe2a67d7962827f27368429f724
Ildar Akhmetgaleev <akhilman@gmail.com>
2022-10-30 02:11:51 +0700

    Use <C-n> and <C-p> to jump between jumpables

--- lua/plug-completion.lua
+++ lua/plug-completion.lua
@@ -41,7 +41,7 @@ function M.packer_setup()
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
-      ['<C-l>'] = cmp.mapping(function(fallback)
+      ['<C-n>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
@@ -50,7 +50,7 @@ function M.packer_setup()
           fallback()
         end
       end, { 'i', 's' }),
-      ['<C-h>'] = cmp.mapping(function(fallback)
+      ['<C-p>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
