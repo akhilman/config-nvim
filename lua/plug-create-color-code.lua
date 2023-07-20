aa3cfec0e951303fe215d5c59338afa786c68c8b
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-07-20 09:17:35 +0700

    Map arrow keys to change color in ccc dialog

--- lua/plug-create-color-code.lua
+++ lua/plug-create-color-code.lua
@@ -2,7 +2,14 @@ local M = {}
 
 function M.packer_startup(use)
   use { 'uga-rosa/ccc.nvim', config = function()
-    require('ccc').setup {}
+    ccc = require('ccc')
+    mapping = ccc.mapping
+    ccc.setup {
+      mappings = {
+        ['<Left>'] = mapping.decrease1,
+        ['<Right>'] = mapping.increase1,
+      }
+    }
   end }
 end
 
