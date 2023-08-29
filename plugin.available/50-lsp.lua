e6e57a55f373588cd0cdef50c407cc5dccb583e0
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-08-30 00:31:40 +0700

    Use LSP's omnifunc if cmp is not installed

--- plugin.available/50-lsp.lua
+++ plugin.available/50-lsp.lua
@@ -13,8 +13,10 @@ local function config()
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
   local function on_attach(client, bufnr) ---@diagnostic disable-line: unused-local
-    -- Enable completion triggered by <c-x><c-o>
-    -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    if not packer_plugins['cmp-mvim-lsp'] then
+      -- Enable completion triggered by <c-x><c-o>
+      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    end
 
     local function hover()
       if packer_plugins['rust-tools.nvim'] and vim.bo[bufnr].filetype == 'rust' then
