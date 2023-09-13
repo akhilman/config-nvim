615b183f3908dac85c88c66007f8388534762823
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-09-13 16:00:01 +0700

    Use config callback to load colorscheme

--- plugin-available/50-colorscheme.lua
+++ plugin-available/50-colorscheme.lua
@@ -3,14 +3,18 @@
 if vim.env.COLORTERM == "truecolor" then
   vim.o.termguicolors = true
 end
-pcall(function() vim.cmd 'colorscheme carbonfox' end)
+-- pcall(function() vim.cmd 'colorscheme carbonfox' end)
 
 local try_use = require('packer_utils').try_use
+try_use {
+  'EdenEast/nightfox.nvim',
+  config = function() vim.cmd 'colorscheme carbonfox' end
+}
+
 try_use 'Mofiqul/adwaita.nvim'
 try_use 'projekt0n/github-nvim-theme'
 try_use { 'metalelf0/jellybeans-nvim',
   requires = { 'rktjmp/lush.nvim' }
 }
 try_use 'rebelot/kanagawa.nvim'
-try_use 'EdenEast/nightfox.nvim'
 try_use 'olimorris/onedarkpro.nvim'
