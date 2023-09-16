ce0ab73b2336cd5cc3fe7ba33a07eb2636fe9468
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-09-17 05:45:39 +0700

    Do not disable netrw by nvimtree

--- plugin-available/50-nvim-tree.lua
+++ plugin-available/50-nvim-tree.lua
@@ -1,6 +1,8 @@
 local function config()
   require("nvim-tree").setup {
-    update_focused_file = { enable = true }
+    update_focused_file = { enable = true },
+    disable_netrw = false,
+    hijack_netrw = false,
   }
   vim.keymap.set(
     'n', '<Leader>t',
