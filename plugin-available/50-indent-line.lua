78d8f79466c488938b8bcc1de0f3a33b0ff53b45
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-09-29 06:38:40 +0700

    Update config for indent-blank-line v3

--- plugin-available/50-indent-line.lua
+++ plugin-available/50-indent-line.lua
@@ -1,12 +1,5 @@
 local function config()
-  local have_treesitter = packer_plugins['nvim-treesitter'] ~= nil
-  require('indent_blankline').setup {
-    -- for example, context is off by default, use this to turn it on
-    -- these options requires treesitter
-    show_current_context = have_treesitter,
-    show_current_context_start = have_treesitter,
-    use_treesitter = have_treesitter,
-  }
+  require('ibl').setup {}
 end
 
 require('packer_bootstrap').use {
