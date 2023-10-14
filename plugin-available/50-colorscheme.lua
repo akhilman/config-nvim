0986512cf5ba3e981b609500f6e1fe8a70ca547d
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-10-14 07:47:35 +0700

    Remove some colour schemes

--- plugin-available/50-colorscheme.lua
+++ plugin-available/50-colorscheme.lua
@@ -1,20 +1,23 @@
 -- Set color scheme
 
-if vim.env.COLORTERM == "truecolor" then
+if vim.env.COLORTERM == 'truecolor' then
   vim.o.termguicolors = true
 end
--- pcall(function() vim.cmd 'colorscheme carbonfox' end)
 
 local use = require('packer_bootstrap').use
+
 use {
   'EdenEast/nightfox.nvim',
-  config = function() vim.cmd 'colorscheme carbonfox' end
+  config = function() vim.cmd.colorscheme 'carbonfox' end
 }
 
-use 'Mofiqul/adwaita.nvim'
-use 'projekt0n/github-nvim-theme'
-use { 'metalelf0/jellybeans-nvim',
-  requires = { 'rktjmp/lush.nvim' }
+use {
+  'projekt0n/github-nvim-theme',
+  -- config = function() vim.cmd.colorscheme 'github_dark_tritanopia' end
 }
-use 'rebelot/kanagawa.nvim'
-use 'olimorris/onedarkpro.nvim'
+
+use {
+  'rebelot/kanagawa.nvim',
+  -- config = function() vim.cmd.colorscheme 'kanagawa' end,
+  -- config = function() vim.cmd.colorscheme 'kanagawa-dragon' end,
+}
