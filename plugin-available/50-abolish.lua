bdd1023d7775703448c7e720c11b66093a6c86c9
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-10-15 09:49:16 +0700

    Add 'Abolish' plugin

---
+++ plugin-available/50-abolish.lua
@@ -0,0 +1,6 @@
+-- Abbriviation and substituton for word variants
+-- https://github.com/tpope/vim-abolish
+-- ``:%S/facilit{y,ies}/building{,s}/g``
+
+local use  = require('packer_bootstrap').use
+use 'tpope/vim-abolish'
