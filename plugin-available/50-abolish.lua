e93bc4156b12d122b3806c4c0482bd888758902e
Ildar Akhmetgaleev <akhilman@gmail.com>
2023-10-18 07:13:00 +0700

    Apply stylua to abolish.lua

--- plugin-available/50-abolish.lua
+++ plugin-available/50-abolish.lua
@@ -2,5 +2,5 @@
 -- https://github.com/tpope/vim-abolish
 -- ``:%S/facilit{y,ies}/building{,s}/g``
 
-local use  = require('packer_bootstrap').use
-use 'tpope/vim-abolish'
+local use = require("packer_bootstrap").use
+use("tpope/vim-abolish")
